f10bef6b09a14416e5e00c290609605465384160
Edwin Agudelo <eagudelo@hotmail.com>
2023-04-02 11:48:21 -0500

    First commit. Here I can call the backend and save info in the database

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,20 +1,53 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, Injectable, OnInit, inject } from '@angular/core';
 import { IonicModule, RefresherCustomEvent } from '@ionic/angular';
 import { MessageComponent } from '../message/message.component';
 
 import { DataService, Message } from '../services/data.service';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Todo, TodoService } from '../services/todo.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, MessageComponent],
+  imports: [IonicModule, CommonModule, MessageComponent, ReactiveFormsModule],
 })
-export class HomePage {
+export class HomePage implements OnInit{
+  todos$!: Todo[];
+  validateForm!: FormGroup;
+  
   private data = inject(DataService);
+  private todoService = inject(TodoService);
+  private fb = inject(FormBuilder)
+
   constructor() {}
+  ngOnInit(): void {
+    this.todoService.findAll().then( (res) => {
+      this.todos$ = res.data;
+    });
+    this.validateForm = this.fb.group({
+      title: [null, Validators.required]
+    });
+  }
+
+  submitForm(value : {title : string, completed :false}): void {
+    for(const key  in this.validateForm.controls){
+      if(this.validateForm.controls.hasOwnProperty(key)){
+        this.validateForm.controls[key].markAsDirty();
+        this.validateForm.controls[key].updateValueAndValidity();
+      }
+    }
+    value.completed = false;
+    console.log("Submitted", value);
+    this.todoService.create(value).then( () =>{
+      this.todoService.findAll().then( (res) => {
+        this.todos$ = res.data;
+      });
+    } );
+    this.validateForm.reset();
+  }
 
   refresh(ev: any) {
     setTimeout(() => {
---
+++ src/app/services/todo.service.ts
@@ -0,0 +1,65 @@
+import { Injectable } from "@angular/core";
+import { Http, HttpResponse } from "@capacitor-community/http";
+import { promises } from "dns";
+
+export interface Todo{
+    id?: number;
+    title: string;
+    completed: boolean;
+}
+@Injectable({
+    providedIn: 'root'
+})
+export class TodoService{
+    private resourceURL = 'https://localhost:5001/api/todocontroller';
+
+    constructor(){}
+
+    create(todo:Todo): Promise<HttpResponse>{
+        const options = {
+            url: `${this.resourceURL}`,
+            data: todo,
+            headers: { 'Content-Type': 'application/json'}
+        };
+
+        return Http.post(options);
+    }
+
+    update(todo:Todo): Promise<HttpResponse>{
+        const options ={
+            url: `${this.resourceURL}/${todo.id}`,
+            data: todo,
+            headers: { 'Content-Type': 'application/json'}
+        };
+        return Http.put(options);
+    }
+
+    findAll():Promise<HttpResponse>{
+        const options ={
+            url: `${this.resourceURL}`,
+            headers: { 'Content-Type': 'application/json'}
+        };
+
+        return Http.get(options);
+    }
+
+    findbyId(id: number) : Promise<HttpResponse>{
+        const options ={
+            url: `${this.resourceURL}/${id}`,
+            headers: { 'Content-Type': 'application/json'}
+        };
+
+        return Http.get(options);
+    }
+
+    delete(id: number) : Promise<HttpResponse> {
+        const options ={
+            url: `${this.resourceURL}/${id}`,
+            headers: { 'Content-Type': 'application/json'}
+        };
+
+        return Http.del(options);
+    }
+
+    
+}
